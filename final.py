ec0836ba809678c2c632841c2a70245d60f4c94b
barbaraskar <71756668+barbaraskar@users.noreply.github.com>
2023-09-26 19:03:16 +0300

    Update final.py
    add comments to the code... 
    more updates soon . staytuned....

--- final.py
+++ final.py
@@ -1,13 +1,21 @@
 import time
 import pyautogui
+# let the programm sleep before start
 time.sleep(5)
 
+
 j=0
+
+# difine where to start drawing 
 x=400
 y=300
+#deffin where is the color palet y cordinates
 b=62
 while j <2:
+    #deffin where is the color palet X cordinates
     a=760
+    
+    
     i=0
     while i<10:
         pyautogui.moveTo(a,b)
@@ -15,6 +23,7 @@ while j <2:
         pyautogui.click()
         i=i+1
         a=a+22
+# un comment next to draw extra shapes....
         '''pyautogui.moveTo(x, y, duration = 1)
         pyautogui.dragRel(100, 0, duration = 1)
         pyautogui.dragRel(0, 100, duration = 1)
@@ -27,6 +36,7 @@ while j <2:
         pyautogui.dragRel(50, -50, duration = 0.5)
         x=x+10
         y=y+10
-    
+    # move the mouse to anothe place in the color palet..
     b=b+22
     j=j+1
+    ## plaes be nise and sub to my youtube chanel...
